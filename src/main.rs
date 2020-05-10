be30e27acae0bbca38666577b5032bb0a6fb0e22
Ryan Liang <rl@ryanl.io>
2020-05-10 16:18:10 -0500

    Add external program installation check in build script

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,23 @@ mod program;
 
 // ============ Import ============
 use program::PROGRAMS;
+use std::process::exit;
 
 
-fn main() {
-    println!("{}", PROGRAMS.assembler);
+fn main(){
+
+    println!("Start building Redstone");
+
+    // Check if all external programs needed are installed
+    println!("External programs needed: {}, {}, {}, {}",
+             PROGRAMS.cargo, PROGRAMS.linker, PROGRAMS.objcopy, PROGRAMS.assembler);
+    match PROGRAMS.check_install_all() {
+        Ok(_)  => println!("All programs installed, proceeding..."),
+        Err(err) => die(1, "cannot find some external programs"),
+    }
+}
+
+fn die(code: i32, msg: &str) -> ! {
+    println!("Error: {}", msg);
+    exit(code);
 }
--- src/program.rs
+++ src/program.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::process::Command;
+
 /** Programs to be used when building */
 pub struct Program {
     pub cargo:     &'static str,
@@ -6,10 +9,39 @@ pub struct Program {
     pub assembler: &'static str,
 }
 
+impl Program {
+
+    /**
+     * Check if all the programs are installed on the host system,
+     * Here we assume if 
+     */
+    #[cfg(target_os = "macos")]
+    pub fn check_install_all(&self) -> io::Result<()> {
+
+        // Coincidentially all of our programs uses this as their version command
+        let version_arg = ["--version"];
+
+        self.check_install(self.cargo, &version_arg)?;
+        self.check_install(self.linker, &version_arg)?;
+        self.check_install(self.objcopy, &version_arg)?;
+        self.check_install(self.assembler, &version_arg)?;
+
+        Result::Ok(())
+    }
+
+    /** Check whether a single program is installed */
+    fn check_install(&self, command: &str, args: &[&str]) -> io::Result<()> {
+
+        // Using .output() to consume stdout from external programs
+        Command::new(command).args(args).output()?;
+        Result::Ok(())
+    }
+}
+
 #[cfg(target_os = "macos")]
 pub static PROGRAMS: Program = Program {
     cargo:     "cargo",
     linker:    "ld.lld",
-    objcopy:   "x86_64-elf-objcopy",
+    objcopy:   "x86_64elf-objcopy",
     assembler: "nasm",
 };
